469dfe2066e631df4274f85a582c0de6df9d48fe
Stefan Stanisev <shadowrageinc@gmail.com>
2021-05-24 21:13:00 +0300

    Formatted code using clang formatter

--- src/client.c
+++ src/client.c
@@ -7,50 +7,55 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-void error(const char *msg){
+void error(const char *msg)
+{
 	perror(msg);
 	exit(1);
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
-	if(argc < 3){
+	if (argc < 3)
+	{
 		fprintf(stderr, "Port Number not provided");
 		exit(1);
 	}
 
-	int sockfd,  portno, n;
+	int sockfd, portno, n;
 	char buffer[2000];
 	char input[100];
 	char inputFiles[100];
 	char *ptr;
-	char dlim[]=" ";
+	char dlim[] = " ";
 	char actionLenght[2];
-	int actionCharNo=0;
+	int actionCharNo = 0;
 
 	struct sockaddr_in serv_addr;
 	struct hostent *server;
 
 	portno = atoi(argv[2]);
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd < 0) {
-		error("Error opening socket");	
+	if (sockfd < 0)
+	{
+		error("Error opening socket");
 	}
 
 	server = gethostbyname(argv[1]);
-	if(server == NULL)
+	if (server == NULL)
 		fprintf(stderr, "Error, no such host");
 
-	bzero((char *) &serv_addr, sizeof(serv_addr));
+	bzero((char *)&serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
-	bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length);
+	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
 	serv_addr.sin_port = htons(portno);
-	if(connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+	if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 		error("Connection failed");
 
 	printf("▁ ▂ ▄ ▅ ▆ ▇ █  🆂 🆃 🅰 🅽 🅼 🅰 🆁 🅺 1.0 █ ▇ ▆ ▅ ▄ ▂ ▁\n-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶\n⌈ MENU:\n-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶\n⌈ 1.USE file\n-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶\n⌈ 2.SORT file\n-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶\n⌈ *file: SEQUENCE1.txt, SEQUENCE2.txt, SEQUENCE3.txt\n| *Algorithms used: #Bubble, #Shell, #Selection, #Merge\n-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶-̶\n");
-	
-	while(1){
+
+	while (1)
+	{
 
 		printf("Client: ");
 		fgets(input, 100, stdin);
@@ -60,29 +65,26 @@ int main(int argc, char *argv[]){
 		actionCharNo = strlen(ptr);
 		sprintf(actionLenght, "%d", actionCharNo);
 
-		strcpy(inputFiles, inputFiles+actionCharNo+1);
+		strcpy(inputFiles, inputFiles + actionCharNo + 1);
 
 		strcpy(buffer, actionLenght);
-		strcpy(buffer+1, ptr);
-		strcpy(buffer+actionCharNo+1, inputFiles);
+		strcpy(buffer + 1, ptr);
+		strcpy(buffer + actionCharNo + 1, inputFiles);
 
 		n = write(sockfd, buffer, sizeof(buffer));
-		if(n < 0)
+		if (n < 0)
 			error("Error on writing");
 
 		bzero(buffer, 2000);
 
 		n = read(sockfd, buffer, sizeof(buffer));
 
-
-		if(n < 0)
+		if (n < 0)
 			error("Error on reading");
 		printf("Server says %s\n", buffer);
 		bzero(buffer, 2000);
 	}
-	
+
 	close(sockfd);
 	return 0;
-
-
 }
--- src/server.c
+++ src/server.c
@@ -6,14 +6,15 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h>
-#include<fcntl.h>
-#include<dirent.h>
+#include <fcntl.h>
+#include <dirent.h>
 #include "sort.h"
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
 
-void error(const char *msg){
+void error(const char *msg)
+{
     perror(msg);
     exit(1);
 }
@@ -21,16 +22,16 @@ void error(const char *msg){
 void printArray(int arr[], int size)
 {
     int i;
-    for (i=0; i < size; i++)
+    for (i = 0; i < size; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
 
-int * intdup(int const * src, size_t len)
+int *intdup(int const *src, size_t len)
 {
-   int * p = malloc(len * sizeof(int));
-   memcpy(p, src, len * sizeof(int));
-   return p;
+    int *p = malloc(len * sizeof(int));
+    memcpy(p, src, len * sizeof(int));
+    return p;
 }
 
 void *connection_handler(void *);
@@ -38,21 +39,20 @@ void useGivenFile(char file[100], int sockfd);
 void benchmarkGivenFile(char file[100], int sockfd);
 void sort(int arr[], int n, int sockfd);
 
-
 typedef struct data
 {
     char algorithmName[20];
     double time;
 } data;
 
-
 int fileDesc;
 int id = 1;
 
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char *argv[]){
-
-    if(argc < 2){
+    if (argc < 2)
+    {
         fprintf(stderr, "Port Number not provided");
         exit(1);
     }
@@ -64,50 +64,50 @@ int main(int argc, char *argv[]){
     socklen_t clilen;
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0) {
-        error("Error opening socket");  
+    if (sockfd < 0)
+    {
+        error("Error opening socket");
     }
 
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+    bzero((char *)&serv_addr, sizeof(serv_addr));
     portno = atoi(argv[1]);
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
 
-    if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
         error("Binding failed");
 
     listen(sockfd, 5);
     clilen = sizeof(cli_addr);
 
     pthread_t thread_id;
-    
 
-    while(newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen)){
-    
-        if(pthread_create( &thread_id , NULL ,  connection_handler , (void*) &newsockfd) < 0){
+    while (newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen))
+    {
+
+        if (pthread_create(&thread_id, NULL, connection_handler, (void *)&newsockfd) < 0)
+        {
             error("could not create thread");
-                    return 1;
+            return 1;
         }
 
-        
-
         int i = strncmp("Bye", buffer, 3);
-        if(i == 0)
-            break;  
+        if (i == 0)
+            break;
     }
 
-    if(newsockfd < 0) {
+    if (newsockfd < 0)
+    {
         error("Accept failed");
-        return 1;   
+        return 1;
     }
 
     close(newsockfd);
     close(sockfd);
     close(thread_id);
     return 0;
-
 }
 
 void *connection_handler(void *socket_desc)
@@ -120,69 +120,73 @@ void *connection_handler(void *socket_desc)
     char file[100];
     int actionLenghtNo;
     char *result;
-    
 
     puts("Client connected.");
 
-    
-     
-    while( (n = read(sockfd , buffer , 2000)) > 0 ) {
+    while ((n = read(sockfd, buffer, 2000)) > 0)
+    {
 
-    strncpy(actionLength, buffer, 2);
-    actionLenghtNo = atoi(actionLength);
-    strncpy(action, buffer+1, actionLenghtNo);
-    strcpy(file, buffer+actionLenghtNo+1);
+        strncpy(actionLength, buffer, 2);
+        actionLenghtNo = atoi(actionLength);
+        strncpy(action, buffer + 1, actionLenghtNo);
+        strcpy(file, buffer + actionLenghtNo + 1);
 
-    result = malloc(sizeof(char) * strlen(file));
+        result = malloc(sizeof(char) * strlen(file));
 
-    strncpy(result, file, strlen(file)-1);
+        strncpy(result, file, strlen(file) - 1);
 
-    if(!strcmp(action, "USE")) 
-        useGivenFile(result, sockfd);
+        if (!strcmp(action, "USE"))
+            useGivenFile(result, sockfd);
 
-    if(!strcmp(action, "SORT")) 
-        benchmarkGivenFile(result, sockfd);
+        if (!strcmp(action, "SORT"))
+            benchmarkGivenFile(result, sockfd);
 
+        bzero(buffer, 2000);
 
-    bzero(buffer, 2000);
-
-    strcpy(buffer, "ok");
+        strcpy(buffer, "ok");
         //write(sockfd , buffer , sizeof(buffer));
-        
-    bzero(buffer, 2000);
-    bzero(actionLength, 2);
-    bzero(action, 10);
-    bzero(file, 100);
+
+        bzero(buffer, 2000);
+        bzero(actionLength, 2);
+        bzero(action, 10);
+        bzero(file, 100);
     }
-     
-    if(n == 0) {
+
+    if (n == 0)
+    {
         puts("Client disconnected");
         fflush(stdout);
     }
-    else if(n == -1) {
+    else if (n == -1)
+    {
         perror("recv failed");
     }
 
     return 0;
-} 
+}
 
-void useGivenFile(char file[100], int sockfd) {
+void useGivenFile(char file[100], int sockfd)
+{
 
     // opening chosen file from the menu
     fileDesc = open(file, O_RDONLY, 0);
     char fail[] = "Failed to open and run file\n";
     char success[] = "Successfully opened the file\n";
-    if(fileDesc == -1) {
+    if (fileDesc == -1)
+    {
         write(sockfd, fail, sizeof(fail));
         printf("failed to open and read file!\n");
-        return ;
-    } else {
+        return;
+    }
+    else
+    {
         write(sockfd, success, sizeof(success));
-        printf("sucessfully opened the file\n");    
+        printf("sucessfully opened the file\n");
     }
 }
 
-void benchmarkGivenFile(char file[100], int sockfd) {
+void benchmarkGivenFile(char file[100], int sockfd)
+{
     char fileBuffer[2048];
     int arr[1000];
     //read(fileDesc, fileBuffer, 2000);
@@ -190,21 +194,22 @@ void benchmarkGivenFile(char file[100], int sockfd) {
 
     // converting data from file and passing it to the array
     int i;
-    for(i=0;i<1001;i++){
-        arr[i]=fileBuffer[i]-'0';
+    for (i = 0; i < 1001; i++)
+    {
+        arr[i] = fileBuffer[i] - '0';
     }
     // Doing the benchmark by calling sort()
     sort(arr, 2048, sockfd);
 
     printf("\n");
     bzero(fileBuffer, 2048);
-    
-    return ;
+
+    return;
 }
 
 void sort(int arr[], int n, int sockfd)
 {
- 
+
     // making dups of the array
     int *shellArray = intdup(arr, n);
     int *selectionArray = intdup(arr, n);
@@ -215,20 +220,18 @@ void sort(int arr[], int n, int sockfd)
     clock_t endBubble = clock();
     double timeBubble = (double)(endBubble - beginBubble) / CLOCKS_PER_SEC;
     //printf("Time for bubble: %f\n", timeBubble);
-    
+
     clock_t beginShell = clock();
     shellSort(shellArray, n);
     clock_t endShell = clock();
     double timeShell = (double)(endShell - beginShell) / CLOCKS_PER_SEC;
     //printf("Time for shell: %f\n", timeShell);
 
-
     clock_t beginSelection = clock();
     selectionSort(selectionArray, n);
     clock_t endSelection = clock();
     double timeSelection = (double)(endSelection - beginSelection) / CLOCKS_PER_SEC;
     //printf("Time for selection: %f\n", timeSelection);
-
 
     clock_t beginHeap = clock();
     heapSort(heapArray, n);
@@ -237,7 +240,6 @@ void sort(int arr[], int n, int sockfd)
     //printf("Time for head: %f\n", timeHeap);
 
     char result[] = "Time for bubble: ";
-    
 
     // write individual results
 
@@ -249,7 +251,7 @@ void sort(int arr[], int n, int sockfd)
     float time[4] = {timeBubble, timeShell, timeSelection, timeHeap};
     float smallest = time[0];
     float biggest = time[0];
-    int index = 0, slow=0;
+    int index = 0, slow = 0;
     char buf[64] = "\nTime: ";
 
     char res1[] = "Best time with Bubble sort: ";
@@ -257,44 +259,48 @@ void sort(int arr[], int n, int sockfd)
     char res3[] = "Best time with Sellection sort: ";
     char res4[] = "Best time with Heap sort: ";
 
-    for (int i = 0; i < 4; i++) {
-      // find the quickest sort
-      if (time[i] < smallest) {
-         smallest = time[i];
-         index = i;
-      }
-      // find the slowest sort
-      if(time[i]>biggest){
-         biggest = time[i];
-         slow = i;
-      }
-   }
-   switch(index) {
+    for (int i = 0; i < 4; i++)
+    {
+        // find the quickest sort
+        if (time[i] < smallest)
+        {
+            smallest = time[i];
+            index = i;
+        }
+        // find the slowest sort
+        if (time[i] > biggest)
+        {
+            biggest = time[i];
+            slow = i;
+        }
+    }
+    switch (index)
+    {
     case 0:
-        
+
         printf("Best time with Bubble sort with %f s\n", smallest);
 
         snprintf(buf, sizeof buf, "%f", smallest);
         strcat(res1, buf);
         write(sockfd, res1, 64);
         break;
-     case 1:
+    case 1:
         printf("Best time with Shell sort with %f s\n", smallest);
-        
+
         snprintf(buf, sizeof buf, "%f", smallest);
         strcat(res2, buf);
         write(sockfd, res2, 64);
         break;
-     case 2:
+    case 2:
         printf("Best time with Selection sort with %f s\n", smallest);
 
         snprintf(buf, sizeof buf, "%f", smallest);
         strcat(res3, buf);
         write(sockfd, res3, 64);
         break;
-     case 3:
+    case 3:
         printf("Best time with Heap sort with %f s\n", smallest);
-        
+
         snprintf(buf, sizeof buf, "%f", smallest);
         strcat(res4, buf);
         write(sockfd, res4, 64);
@@ -306,10 +312,10 @@ void sort(int arr[], int n, int sockfd)
         strcat(res1, buf);
         write(sockfd, res1, 64);
         break;
-   }
+    }
 
     data algos[4] = {bubble, shell, selection, heap};
-    
+
     ///////////////////////////////////////////////////////
     // Write result to file -> had a problem with system call
     /*
@@ -320,19 +326,22 @@ void sort(int arr[], int n, int sockfd)
 
     FILE *output;
     output = fopen("data.txt", "w");
-    if (output == NULL) {
-      fprintf(stderr, "\nError to open the output file\n");
-      exit (1);
-   }
+    if (output == NULL)
+    {
+        fprintf(stderr, "\nError to open the output file\n");
+        exit(1);
+    }
 
-   char buffer_out[256];
+    char buffer_out[256];
 
-   for(int i=0; i<4;i++){
-    if(i!=slow){
-          sprintf(buffer_out, "%d,%s,%f\n", i, algos[i].algorithmName, algos[i].time);
-          fwrite(buffer_out, sizeof(char), strlen(buffer_out), output);
+    for (int i = 0; i < 4; i++)
+    {
+        if (i != slow)
+        {
+            sprintf(buffer_out, "%d,%s,%f\n", i, algos[i].algorithmName, algos[i].time);
+            fwrite(buffer_out, sizeof(char), strlen(buffer_out), output);
         }
-   }
+    }
 
     fclose(output);
 }
